9d4563fd056acd176c7efc18b4616d1414907ee1
Kristofer Svärd <kristofer.svard@gmail.com>
2016-06-06 19:35:25 +0200

    Fix compiler warnings
    Update iso8601 dependency.

--- apps/erlucia/src/erlucia_consumer.erl
+++ apps/erlucia/src/erlucia_consumer.erl
@@ -6,7 +6,7 @@
 
 -export([start_link/0]).
 %% Callbacks
--export([init/1, handle_info/2, terminate/2]).
+-export([init/1, handle_call/3, handle_cast/2, handle_info/2, terminate/2, code_change/3]).
 
 -define(ROUTING_KEY, <<"light-level">>).
 -define(EXCHANGE, <<"automation">>).
@@ -27,23 +27,18 @@ handle_info(connect_amqp, _State) ->
     Queuename = queue_name("erlucia"),
     {ok, Connection} = amqp_connection:start(#amqp_params_network{host = Host}),
     {ok, Channel} = amqp_connection:open_channel(Connection),
-
     #'exchange.declare_ok'{} = amqp_channel:call(Channel, #'exchange.declare'{exchange = ?EXCHANGE}),
-
     #'queue.declare_ok'{} = amqp_channel:call(Channel, #'queue.declare'{queue = Queuename,
                                                                         auto_delete = true}),
-
     #'queue.bind_ok'{} = amqp_channel:call(Channel, #'queue.bind'{exchange = ?EXCHANGE,
                                                                   queue = Queuename,
                                                                   routing_key = ?ROUTING_KEY}),
-
     Sub = #'basic.consume'{queue = Queuename, no_ack = true},
     amqp_channel:call(Channel, Sub),
-    
     {noreply, #state{connection = Connection, channel = Channel}};
 handle_info(#'basic.consume_ok'{}, State) ->
     {noreply, State};
-handle_info(#'basic.cancel_ok'{}, State) ->
+handle_info(#'basic.cancel_ok'{}, _State) ->
     {stop, normal, undefined};
 handle_info({#'basic.deliver'{delivery_tag = _Tag}, #amqp_msg{payload = Payload}}, State) ->
     Pid = spawn(erlucia_controller, handle_payload, [Payload]),
@@ -63,6 +58,15 @@ terminate(_Reason, #state{connection = Connection, channel = Channel}) ->
     lager:info("Disconnecting from message bus and terminating consumer"),
     amqp_channel:close(Channel),
     amqp_connection:close(Connection).
+
+handle_call(_Request, _From, State) ->
+    {noreply, State}.
+
+handle_cast(_Request, State) ->
+    {noreply, State}.
+
+code_change(_OldVsn, State, _Extra) ->
+    {ok, State}.
 
 %% Private
 queue_name(Name) ->
--- apps/erlucia/src/erlucia_controller.erl
+++ apps/erlucia/src/erlucia_controller.erl
@@ -6,16 +6,18 @@ handle_payload(Payload) ->
     Json = jsx:decode(Payload, [return_maps]),
     check(Json).
 
+%% Private
 check(#{<<"date">> := Date, <<"level">> := Level}) when Level < 0.1 ->
     Datetime = iso8601:parse(Date),
     {_, {Hour, _, _}} = calendar:universal_time_to_local_time(Datetime),
     lager:info("Level is under threshold: ~p", [Level]),
     step_fsm(Hour);
-check(#{<<"date">> := Date, <<"level">> := Level}) ->
+check(#{<<"level">> := Level}) ->
     lager:info("Level is above threshold: ~p", [Level]),
     erlucia_fsm:reset().
 
+%% Private
 step_fsm(Hour) when (Hour > 12) and (Hour < 23) ->
     erlucia_fsm:next();
 step_fsm(_Hour) ->
-    ignored.
+    ignore.
